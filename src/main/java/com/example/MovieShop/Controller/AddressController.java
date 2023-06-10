734c2700ee0399c5804310620d370fbc87b16ec8
PiotrLarkowski <p.larkowski90@gmail.com>
2023-06-10 18:55:08 +0200

    End AddressContoroller

--- src/main/java/com/example/MovieShop/Controller/AddressController.java
+++ src/main/java/com/example/MovieShop/Controller/AddressController.java
@@ -7,24 +7,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/address")
 public class AddressController {
     private final AddressService addressService;
-
     public AddressController(AddressService addressService) {
         this.addressService = addressService;
     }
-
     @PostMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Address createAddress(@RequestBody @Validated AddressDto addressDto) throws Exception{
         return addressService.createAddress(addressDto);
     }
-
     @PutMapping(path = "/{id}")
     public void updateAddress(@RequestBody @Validated AddressDto addressDto, @PathVariable Long id) {
         addressService.updateAddress(addressDto, id);
     }
-
+    @GetMapping(path = "/getAll")
+    public List<Address> getAllAddresses(){
+        return addressService.getAllAddress();
+    }
+    @GetMapping(path = "/{id}")
+    public Address getAddressById(@PathVariable Long id){
+        return addressService.getAddress(id);
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deleteAddressById(@PathVariable Long id){
+        addressService.deleteAddress(id);
+    }
 }
