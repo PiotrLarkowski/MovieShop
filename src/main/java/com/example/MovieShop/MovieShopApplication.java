f3fe75ae759f30e33d4ba45bea30109e10234bfc
PiotrLarkowski <p.larkowski90@gmail.com>
2023-06-22 21:51:56 +0200

    specyfy Swagger documentation information

--- src/main/java/com/example/MovieShop/MovieShopApplication.java
+++ src/main/java/com/example/MovieShop/MovieShopApplication.java
@@ -8,10 +8,17 @@ import com.example.MovieShop.Services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -47,5 +54,24 @@ public class MovieShopApplication implements CommandLineRunner {
 		MovieDto movieDto = new MovieDto(new ArrayList<Actor>(Arrays.asList(newActor)),"Ryzykowny Interes","Pod nieobecność rodziców nastolatek Joel poznaje kobietę lekkich obyczajów, Lanę, i za jej namową urządza w miejscu zamieszkania... dom publiczny.", MoviesGenres.COMEDY);
 		Movie movie = movieService.CreateMovie(movieDto);
 		movieRentService.createMovieRent(movie.getMovieId(),client.getClientId());
+	}
+
+	@Bean
+	public Docket get(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.build().apiInfo(createApiInfo());
+	}
+
+	private ApiInfo createApiInfo() {
+		return new ApiInfo("MovieShopApi",
+				"Rental movie database",
+				"1.00",
+				"",
+				new Contact("Piotr","https://www.linkedin.com/in/piotr-larkowski/","p.larkowski90@gmail.com"),
+				"My own licence",
+				"My own licence",
+				Collections.emptyList()
+		);
 	}
 }
